b17e41a7cc4222f749bab59e575647662ff07160
Isaac <30970819+yuyoux@users.noreply.github.com>
2019-03-15 04:15:43 +1100

    add build file

---
+++ build/precache-manifest.49ea10025ea7f6b78a629bb82a76a299.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c73e109622acf0d699",
+    "url": "/static/js/main.6cd7ce0f.chunk.js"
+  },
+  {
+    "revision": "69dbfa48a9bbe90811e3",
+    "url": "/static/js/2.bc99a410.chunk.js"
+  },
+  {
+    "revision": "b7c73e109622acf0d699",
+    "url": "/static/css/main.a8a3d81a.chunk.css"
+  },
+  {
+    "revision": "9223c9741053dfb25a7e587cea280920",
+    "url": "/index.html"
+  }
+];
